410979629c997973cb01cf257d2198f29eaed150
Irina Tregub <irinatregub@gmail.com>
2022-01-23 20:11:16 +0200

    Calculator task completed

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {SafeAreaView, TextInput, StyleSheet, Text, Button, View } from 'react-native';
 
 export default function App() {
+  const [numberOne, onChangeNumberOne] = useState(null);
+  const [numberTwo, onChangeNumberTwo] = useState(null);
+  const [result, setResult] = useState("");
+  
+  const onClickSum = () => {
+    if(numberOne != null && numberTwo != null){
+      const sum = parseInt(numberOne) + parseInt(numberTwo);
+      setResult(sum);
+    }
+    
+  }
+
+  const onClickDeduction = () => {
+    if(numberOne != null && numberTwo != null){
+      const deduction = parseInt(numberOne) - parseInt(numberTwo);
+      setResult(deduction);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.areaView}>
+      <Text>Result:{result} </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumberOne}
+        value={numberOne}
+        keyboardType="number-pad"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumberTwo}
+        value={numberTwo}
+        keyboardType="number-pad"
+      />
+      <View style={styles.fixToText}>
+        <Button onPress={onClickSum} title="+" color="#f194ff"  ></Button>
+      </View>
+      <View style={styles.fixToText}>
+        <Button onPress={onClickDeduction} title="-"></Button>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
+
+  areaView: {
+    marginTop: 50,
+
+  },
+
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }
+
 });
